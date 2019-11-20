32b1f2cf92d14f156ad1dfaa57c37668972e25af
tonzy3 <tonzr3@yahoo.com>
2019-11-20 02:19:37 -0500

    forecast

--- src/pages/forecast.js
+++ src/pages/forecast.js
@@ -3,18 +3,23 @@ import TableauReport from 'tableau-react';
 import {Link} from "react-router-dom";
 
 class Forecast extends Component {
+
     render() {
         const options = {
-            hideTabs: true,
-            hideToolbar: true
+            //hideTabs: true,
+            //hideToolbar: true
             // All other vizCreate options are supported here, too
             // They are listed here: https://onlinehelp.tableau.com/current/api/js_api/en-us/JavaScriptAPI/js_api_ref.htm#ref_head_9
         };
         return (
             <div className='Forecast'>
             <Link to='/'><button>Home</button></Link>
+                        <TableauReport
+                url="https://public.tableau.com/views/forcastofNYCairbnbpriceandavaliabilityforNYCandmanhattan/Sheet8?:display_count=y&:origin=viz_share_link"
+                options={options}
+            />
             <TableauReport
-                url="https://public.tableau.com/views/forcastofNYCairbnbpriceandavaliabilityforNYCandmanhattan/Dashboard1"
+                url="https://public.tableau.com/views/forcastofNYCairbnbpriceandavaliabilityforNYCandmanhattan/Sheet82?:display_count=y&:origin=viz_share_link"
                 options={options}
             />
             </div>
